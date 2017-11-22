430160b364ed6c95161129e8f34ca53965c1796a
Italo Lelis de Vietro <italolelis@gmail.com>
2017-11-22 14:48:58 +0100

    imrpoved error handling

--- cmd/create_repo.go
+++ cmd/create_repo.go
@@ -1,9 +1,8 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/fatih/color"
+	"github.com/google/go-github/github"
 	"github.com/hellofresh/github-cli/pkg/pullapprove"
 	"github.com/hellofresh/github-cli/pkg/repo"
 	"github.com/pkg/errors"
@@ -24,6 +23,9 @@ type (
 		HasDefaultLabels     bool
 		HasWebhooks          bool
 		HasBranchProtections bool
+		HasIssues            bool
+		HasWiki              bool
+		HasPages             bool
 	}
 )
 
@@ -36,28 +38,32 @@ func init() {
 	createRepoCmd.Flags().StringVarP(&createRepoFlags.Description, "description", "d", "", "The repository's description")
 	createRepoCmd.Flags().StringVarP(&createRepoFlags.Org, "organization", "o", "", "Github's organization")
 	createRepoCmd.Flags().BoolVar(&createRepoFlags.Private, "private", true, "Is the repository private?")
-	createRepoCmd.Flags().BoolVar(&createRepoFlags.HasPullApprove, "add-pullapprove", true, "Enables pull approve")
-	createRepoCmd.Flags().BoolVar(&createRepoFlags.HasTeams, "add-teams", true, "Enable teams")
-	createRepoCmd.Flags().BoolVar(&createRepoFlags.HasLabels, "add-labels", true, "Enable labels")
-	createRepoCmd.Flags().BoolVar(&createRepoFlags.HasDefaultLabels, "add-default-labels", true, "Removes the default github labels")
-	createRepoCmd.Flags().BoolVar(&createRepoFlags.HasWebhooks, "add-webhooks", true, "Enables webhooks configurations")
-	createRepoCmd.Flags().BoolVar(&createRepoFlags.HasBranchProtections, "add-branch-protections", true, "Enables branch protections")
+
+	createRepoCmd.Flags().BoolVar(&createRepoFlags.HasIssues, "has-issues", true, "Enables issue pages")
+	createRepoCmd.Flags().BoolVar(&createRepoFlags.HasWiki, "has-wiki", false, "Enables wiki pages?")
+	createRepoCmd.Flags().BoolVar(&createRepoFlags.HasPages, "has-pages", false, "Enables github pages?")
+	createRepoCmd.Flags().BoolVar(&createRepoFlags.HasPullApprove, "has-pullapprove", true, "Enables pull approve")
+	createRepoCmd.Flags().BoolVar(&createRepoFlags.HasTeams, "has-teams", true, "Enable teams")
+	createRepoCmd.Flags().BoolVar(&createRepoFlags.HasLabels, "has-labels", true, "Enable labels")
+	createRepoCmd.Flags().BoolVar(&createRepoFlags.HasDefaultLabels, "rm-default-labels", true, "Removes the default github labels")
+	createRepoCmd.Flags().BoolVar(&createRepoFlags.HasWebhooks, "has-webhooks", false, "Enables webhooks configurations")
+	createRepoCmd.Flags().BoolVar(&createRepoFlags.HasBranchProtections, "has-branch-protections", true, "Enables branch protections")
 }
 
 // RunCreateRepo runs the command to create a new repository
 func RunCreateRepo(cmd *cobra.Command, args []string) {
 	var err error
+	var org string
 
 	name := createRepoFlags.Name
 	checkEmpty(name, "Please provide a repository name")
 
 	description := createRepoFlags.Description
 
-	org := createRepoFlags.Org
+	org = createRepoFlags.Org
 	if org == "" {
 		org = globalConfig.Github.Organization
 	}
-	checkEmpty(org, "Please provide an organization")
 
 	opts := &repo.GithubRepoOpts{
 		PullApprove: &repo.PullApproveOpts{
@@ -77,7 +83,15 @@ func RunCreateRepo(cmd *cobra.Command, args []string) {
 	creator := repo.NewGithub(githubClient)
 
 	color.White("Creating repository...")
-	ghRepo, err := creator.CreateRepo(name, description, org, createRepoFlags.Private)
+	ghRepo, err := creator.CreateRepo(org, &github.Repository{
+		Name:        github.String(name),
+		Description: github.String(description),
+		Private:     github.Bool(createRepoFlags.Private),
+		HasIssues:   github.Bool(createRepoFlags.HasIssues),
+		HasWiki:     github.Bool(createRepoFlags.HasWiki),
+		HasPages:    github.Bool(createRepoFlags.HasPages),
+		AutoInit:    github.Bool(true),
+	})
 	if errors.Cause(err) == repo.ErrRepositoryAlreadyExists {
 		color.Cyan("Repository already exists. Trying to normalize it...")
 	} else {
@@ -122,16 +136,19 @@ func RunCreateRepo(cmd *cobra.Command, args []string) {
 		if errors.Cause(err) == repo.ErrWebhookAlreadyExist {
 			color.Cyan("Webhook already exists, moving on...")
 		} else {
-			checkEmpty(errors.Wrap(err, "could add webhooks to repository"), "")
+			checkEmpty(errors.Wrap(err, "could not add webhooks to repository"), "")
 		}
 	}
 
 	if createRepoFlags.HasBranchProtections {
 		color.White("Adding branch protections to repository...")
 		err = creator.AddBranchProtections(name, org, opts.BranchProtections)
-		checkEmpty(errors.Wrap(err, "could add branch protections to repository"), "")
+		checkEmpty(errors.Wrap(err, "could not add branch protections to repository"), "")
 	}
 
-	color.Green(fmt.Sprintf("Repository created! \n Here is how to access it %s", *ghRepo.URL))
-	checkEmpty(err, "Could not create github repo")
+	if ghRepo != nil {
+		color.Green("Repository created! \n Here is how to access it %s", *ghRepo.URL)
+	} else {
+		color.Green("Repository normalized!")
+	}
 }
