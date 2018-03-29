537da3f6f9458b281a2a61f30006fb49aed8b7e8
Italo Lelis de Vietro <italolelis@gmail.com>
2018-03-29 12:15:20 +0200

    Using context

--- cmd/version.go
+++ cmd/version.go
@@ -1,25 +1,23 @@
 package cmd
 
 import (
-	log "github.com/sirupsen/logrus"
+	"context"
+
+	"github.com/hellofresh/github-cli/pkg/log"
 	"github.com/spf13/cobra"
 )
 
 var version = "0.0.0-dev"
 
 // NewVersionCmd creates a new version command
-func NewVersionCmd() *cobra.Command {
+func NewVersionCmd(ctx context.Context) *cobra.Command {
 	return &cobra.Command{
 		Use:     "version",
 		Short:   "Print the version information",
 		Aliases: []string{"v"},
 		Run: func(cmd *cobra.Command, args []string) {
-			RunVersion()
+			logger := log.WithContext(ctx)
+			logger.Infof("github-cli %s", version)
 		},
 	}
-}
-
-// RunVersion runs the command to print the current version
-func RunVersion() {
-	log.Infof("github-cli %s", version)
 }
