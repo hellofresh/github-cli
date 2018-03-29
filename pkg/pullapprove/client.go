dc1423f059b00721e06367b9de02771ccc52a5b5
Italo Lelis de Vietro <italolelis@gmail.com>
2018-03-29 14:27:38 +0200

    Bulding a proper client

--- pkg/pullapprove/client.go
+++ pkg/pullapprove/client.go
@@ -1,19 +1,25 @@
 package pullapprove
 
 import (
-	"bytes"
 	"errors"
 	"fmt"
 	"net/http"
 
+	"github.com/dghubble/sling"
 	"github.com/hashicorp/errwrap"
 )
 
-// Client represents a pull approve client
-type Client struct {
-	BaseURL string
-	Token   string
-}
+type (
+	// Client represents a pull approve client
+	Client struct {
+		Token  string
+		client *sling.Sling
+	}
+
+	createReq struct {
+		Name string `json:"name"`
+	}
+)
 
 var (
 	// ErrPullApproveUnauthorized is used when we receive a 401 from pull approve
@@ -26,32 +32,48 @@ var (
 // New creates a new instance of Client
 func New(token string) *Client {
 	return &Client{
-		BaseURL: "https://pullapprove.com/api",
-		Token:   token,
+		Token:  token,
+		client: sling.New().Base("https://pullapprove.com/api/"),
 	}
 }
 
 // Create creates a new repository on pull approve
-func (c *Client) Create(name string, organization string) error {
-	body := []byte(fmt.Sprintf(`{"name": "%s"}`, name))
-	req, err := http.NewRequest("POST", fmt.Sprintf("%s/orgs/%s/repos/", c.BaseURL, organization), bytes.NewBuffer(body))
+func (c *Client) Create(name string, org string) error {
+	path := fmt.Sprintf("orgs/%s/repos/", org)
+	cr := createReq{Name: name}
+
+	req, err := c.client.Post(path).BodyJSON(&cr).Request()
 	if err != nil {
 		return errwrap.Wrapf("could not create the request to pull approve: {{err}}", err)
 	}
 
-	req.Header.Add("Authorization", fmt.Sprintf("Token %s", c.Token))
-	req.Header.Add("Content-Type", "application/json")
+	return c.doRequest(req)
+}
 
-	res, err := http.DefaultClient.Do(req)
+// Delete deletes a repository from pull approve
+func (c *Client) Delete(name string, org string) error {
+	path := fmt.Sprintf("orgs/%s/%s/delete", org, name)
+	req, err := c.client.Post(path).Request()
 	if err != nil {
-		return errwrap.Wrapf("could not create a pull approve repository: {{err}}", err)
+		return errwrap.Wrapf("could not delete the request from pull approve: {{err}}", err)
 	}
 
-	if res.StatusCode == http.StatusUnauthorized {
+	return c.doRequest(req)
+}
+
+func (c *Client) doRequest(req *http.Request) error {
+	req.Header.Add("Authorization", fmt.Sprintf("Token %s", c.Token))
+
+	resp, err := c.client.Do(req, nil, nil)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode == http.StatusUnauthorized {
 		return ErrPullApproveUnauthorized
 	}
 
-	if res.StatusCode != http.StatusCreated {
+	if code := resp.StatusCode; code >= 400 {
 		return ErrPullApproveServerError
 	}
 
