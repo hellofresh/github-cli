b2dc83ea62fe2ee3afdd5d7693cba8d2c847e4e3
Josiah Adenegan <ja@hellofresh.com>
2019-08-16 15:21:48 +0200

    fix version injection for build.sh

--- build/build.sh
+++ build/build.sh
@@ -12,7 +12,7 @@ fi
 echo "Building application version $VERSION"
 
 echo "Building default binary"
-CGO_ENABLED=0 go build -ldflags "-s -w" -ldflags "-X cmd.version=${VERSION}" -o "dist/github-cli" $PKG_SRC
+CGO_ENABLED=0 go build -ldflags "-s -w" -ldflags "-X github.com/hellofresh/github-cli/cmd.version=${VERSION}" -o "dist/github-cli" $PKG_SRC
 
 # Build 386 amd64 binaries
 OS_PLATFORM_ARG=(linux darwin windows)
@@ -20,7 +20,7 @@ OS_ARCH_ARG=(386 amd64)
 for OS in ${OS_PLATFORM_ARG[@]}; do
   for ARCH in ${OS_ARCH_ARG[@]}; do
     echo "Building binary for $OS/$ARCH..."
-    GOARCH=$ARCH GOOS=$OS CGO_ENABLED=0 go build -ldflags "-s -w" -ldflags "-X cmd.version=${VERSION}" -o "dist/github-cli_$OS-$ARCH" $PKG_SRC
+    GOARCH=$ARCH GOOS=$OS CGO_ENABLED=0 go build -ldflags "-s -w" -ldflags "-X github.com/hellofresh/github-cli/cmd.version=${VERSION}" -o "dist/github-cli_$OS-$ARCH" $PKG_SRC
   done
 done
 
@@ -30,6 +30,6 @@ OS_ARCH_ARG=(arm arm64)
 for OS in ${OS_PLATFORM_ARG[@]}; do
   for ARCH in ${OS_ARCH_ARG[@]}; do
     echo "Building binary for $OS/$ARCH..."
-    GOARCH=$ARCH GOOS=$OS CGO_ENABLED=0 go build -ldflags "-s -w" -ldflags "-X cmd.version=${VERSION}" -o "dist/github-cli_$OS-$ARCH" $PKG_SRC
+    GOARCH=$ARCH GOOS=$OS CGO_ENABLED=0 go build -ldflags "-s -w" -ldflags "-X github.com/hellofresh/github-cli/cmd.version=${VERSION}" -o "dist/github-cli_$OS-$ARCH" $PKG_SRC
   done
 done
